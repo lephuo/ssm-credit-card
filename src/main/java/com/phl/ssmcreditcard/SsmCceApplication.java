26ae232702db044588529b85b8f4ec75a33ae6af
Phuong LE <phuonghoangle@gmail.com>
2022-11-15 18:02:56 +0700

    Init spring project

---
+++ src/main/java/com/phl/ssmcreditcard/SsmCceApplication.java
@@ -0,0 +1,13 @@
+package com.phl.ssmcreditcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmCceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsmCceApplication.class, args);
+    }
+
+}
