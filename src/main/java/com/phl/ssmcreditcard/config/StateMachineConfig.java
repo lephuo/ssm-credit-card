55ad9891ca9ef52f896d0604f68c94976f615176
Phuong LE <phuonghoangle@gmail.com>
2022-11-16 10:09:01 +0700

    Event Listener

--- src/main/java/com/phl/ssmcreditcard/config/StateMachineConfig.java
+++ src/main/java/com/phl/ssmcreditcard/config/StateMachineConfig.java
@@ -6,10 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.listener.StateMachineListenerAdapter;
+import org.springframework.statemachine.state.State;
 
 import java.util.EnumSet;
+import java.util.Optional;
 
 @Slf4j
 @EnableStateMachineFactory
@@ -36,4 +40,18 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
             .and()
             .withExternal().event(PaymentEvent.PRE_AUTH_DECLINED).source(PaymentState.NEW).target(PaymentState.PRE_AUTH_ERROR);
     }
+
+    @Override
+    public void configure(StateMachineConfigurationConfigurer<PaymentState, PaymentEvent> config) throws Exception {
+        config.withConfiguration().listener(listener);
+    }
+
+    private final StateMachineListenerAdapter<PaymentState, PaymentEvent> listener = new StateMachineListenerAdapter<>() {
+        @Override
+        public void stateChanged(State<PaymentState, PaymentEvent> from, State<PaymentState, PaymentEvent> to) {
+            System.out.printf("State changed from %s to %s%n",
+                Optional.ofNullable(from).map(State::getId).orElse(null),
+                to.getId());
+        }
+    };
 }
